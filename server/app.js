12f354ad6a4efaf8c71a14cd2a0b2972eb272f65
Lennon-0 <LennonEhare@Gmail.com>
2025-11-14 14:31:38 -0500

    secret domo key removed.. it shouldnt break anything

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ redisClient.connect().then(() => {
     store: new RedisStore({
       client: redisClient,
     }),
-    secret: 'Domo Arigato', // is this a domo thing?
+    secret: 'Arigato', // is this a domo thing?
     resave: false,
     saveUninitialized: false,
   }));
